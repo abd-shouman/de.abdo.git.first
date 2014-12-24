6e00875c7190725696ac09bfccc9040e47592f96
Abdelrahman Shouman <amsh-1992@hotmail.com>
2014-12-24 13:31:21 +0300

    Changes from DummyBranch is being commited

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -3,6 +3,8 @@ package de.vogella.git.first;
 public class GitTest {
 
 	public static void main(String[] args) {
+		//This comment was added in the DummyBranch
+		//Hello world
 		System.out.println("Git is fun");
 	}
 
